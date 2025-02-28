3daf7721241a99a1f011f7896d5638829d07187c
Guilherme Pagio <116508581+gPagio@users.noreply.github.com>
2025-02-28 18:27:26 -0300

    feat: secao 4 28/02/2025 18:27

---
+++ 4_CSS_REACT/css/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+import MyComponent from './components/MyComponent'
+
+function App() {
+
+  return (
+    <div>
+      {/* CSS Global*/}
+      <h1>React com CSS</h1>
+
+      {/* CSS de Componente */}
+      <MyComponent></MyComponent>
+      <p>Este parágrafo é do App.js</p>
+    </div>
+  )
+}
+
+export default App
