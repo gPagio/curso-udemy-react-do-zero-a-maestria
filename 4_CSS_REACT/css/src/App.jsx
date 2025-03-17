7a06b974b3d1a7b433b8203d30fcd200f543c47e
gPagio <guilhermepagio.contato@gmail.com>
2025-03-17 13:12:19 -0300

    feat: secao 4 finalizacao

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -1,8 +1,14 @@
-import './App.css'
+import "./App.css";
 
-import MyComponent from './components/MyComponent'
+import MyComponent from "./components/MyComponent";
+import Title from "./components/Title";
+
+import { useState } from "react";
 
 function App() {
+  const [n, setN] = useState(10);
+  const [name, setName] = useState("Guilherme");
+  const [redTitle, setRedTitle] = useState(true);
 
   return (
     <div>
@@ -12,8 +18,37 @@ function App() {
       {/* CSS de Componente */}
       <MyComponent></MyComponent>
       <p>Este parágrafo é do App.js</p>
+
+      {/* Inline CSS */}
+      <p
+        style={{ color: "blue", padding: "25px", borderTop: "10px solid red" }}
+      >
+        Este elemento foi estilizado de forma inline
+      </p>
+
+      {/* Inline CSS Dinâmico */}
+      <p style={n > 10 ? { color: "magenta" } : { color: "blue" }}>
+        Este elemento foi estilizado de forma inline
+      </p>
+      <p style={n < 10 ? { color: "magenta" } : { color: "blue" }}>
+        Este elemento foi estilizado de forma inline
+      </p>
+      <p
+        style={name === "Guilherme" ? { color: "Green" } : { color: "yellow" }}
+      >
+        Guilherme
+      </p>
+
+      {/* CSS com Classe Dinâmica */}
+      <h2 className={redTitle ? "red-title" : "title"}>
+        Este título tem classe dinamica
+      </h2>
+
+      {/* CSS Módules */}
+      <Title></Title>
+      <h2 className="my_title">Titulo</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
