38051f62ee52144e43245fa29cf1e8c9cc75305e
gPagio <guilhermepagio.contato@gmail.com>
2025-03-24 00:31:31 -0300

    feat: secao 5 cria parte inicial do jogo
    Parte responsavel por carregar e preparar o jogo para o seu inicio

--- 6_SECRET_WORD/secretword/src/App.jsx
+++ 6_SECRET_WORD/secretword/src/App.jsx
@@ -19,16 +19,56 @@ const stages = [
 ];
 
 function App() {
-  const [gameStage, setStage] = useState(stages[0].name);
+  const [gameStage, setGameStage] = useState(stages[0].name);
   const [words, setWords] = useState(wordsList);
 
-  console.log(words);
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndPickCategory = () => {
+    // ESCOLHENDO CATEGORIA ALEATÓRIA
+    const categories = Object.keys(words);
+    const categorie = categories[Math.floor(Math.random() * categories.length)];
+
+    // ESCOLHENDO PALAVRA ALEATÓRIA NA CATEGORIA
+    const wordsInCategory = words[categorie];
+    const word = wordsInCategory[Math.floor(Math.random() * wordsInCategory.length)];
+
+    return {word, categorie};
+  };
+
+  // INICIA O JOGO
+  const startGame = () => {
+    const { word, categorie } = pickWordAndPickCategory();
+
+    const wordLetters = word.split("").map((letra) => removerAcentos(letra.toLowerCase()));
+    setPickedWord(word);
+    setPickedCategory(categorie);
+    setLetters(wordLetters);
+
+    setGameStage(stages[1].name);
+  };
+
+  // REINICIA O JOGO
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
+
+  // PROCESSA INPUT DE LETRAS
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  function removerAcentos(texto) {
+    return texto.normalize("NFD").replace(/\p{Diacritic}/gu, "");
+}
 
   return (
     <div className="App">
-      {gameStage === "start" && <StartScreen></StartScreen>}
-      {gameStage === "game" && <Game></Game>}
-      {gameStage === "end" && <GameOver></GameOver>}
+      {gameStage === "start" && <StartScreen startGame={startGame}></StartScreen>}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter}></Game>}
+      {gameStage === "end" && <GameOver retry={retry}></GameOver>}
     </div>
   );
 }
