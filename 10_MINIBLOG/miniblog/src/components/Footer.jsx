152bda46709db8688ee2958848be261146660bab
gPagio <guilhermepagio.contato@gmail.com>
2025-04-21 10:01:32 -0300

    feat: secao 10 CSS navbar e footer

--- 10_MINIBLOG/miniblog/src/components/Footer.jsx
+++ 10_MINIBLOG/miniblog/src/components/Footer.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+// Css
+import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className={styles.footer}>
+      <h3>Escreva sobre o que você tem interesse!</h3>
+      <p>Mini Blog &copy; {year}</p>
+    </footer>
+  );
 };
 
 export default Footer;
--- 10_MINIBLOG/miniblog/src/components/Navbar.jsx
+++ 10_MINIBLOG/miniblog/src/components/Navbar.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+// Css
+import styles from "./Navbar.module.css";
+
+// React Router
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className={styles.navbar}>
+      <NavLink to="/" className={styles.brand}>
+        Mini <span>Blog</span>
+      </NavLink>
+      <ul className={styles.links_list}>
+        <li>
+          <NavLink to="/" className={({isActive}) => (isActive ? styles.active : '')}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : '')}>About</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
