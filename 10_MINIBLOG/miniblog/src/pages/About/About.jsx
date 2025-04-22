f365a0839119fbf00021f7fea5aa5577bd0dd481
gPagio <guilhermepagio.contato@gmail.com>
2025-04-22 13:53:23 -0300

    feat: secao 10 Bloqueando paginas que precisam de autenticacao

--- 10_MINIBLOG/miniblog/src/pages/About/About.jsx
+++ 10_MINIBLOG/miniblog/src/pages/About/About.jsx
@@ -1,10 +1,15 @@
 // Css
 import styles from "./About.module.css";
 
+// React Router
+import { Link } from "react-router-dom";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>Sobre o Mini <span>Blog</span></h2>
+      <p>Este projeto consiste em um blog feito com React no frontend e Firebase no backend.</p>
+      <Link to="/posts/create" className="btn">Criar Post</Link>
     </div>
   );
 };
