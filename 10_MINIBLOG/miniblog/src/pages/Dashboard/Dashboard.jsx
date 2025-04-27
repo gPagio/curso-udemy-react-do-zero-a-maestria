1c9e9bfd44adba43d1a85ea94f1cc76d66038bb3
gPagio <guilhermepagio.contato@gmail.com>
2025-04-27 17:27:57 -0300

    feat: secao 10 Estruturando dashboard

--- 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.jsx
+++ 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,29 @@
 // Css
 import styles from "./Dashboard.module.css";
 
+// React
+import { Link } from "react-router-dom";
+
+// Hooks
+import { useAuthContextValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 const Dashboard = () => {
+  const posts = [];
+
   return (
     <div>
       <h2>Dashboard</h2>
+      <p>Gerencia os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <Link to="/posts/create" className="btn">Criar Primeiro Post</Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts!</p>
+        </div>
+      )}
     </div>
   );
 };
