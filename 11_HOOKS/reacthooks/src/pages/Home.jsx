96bd2aff39109da7508f49fcf9f7ab6d96b2aac5
gPagio <guilhermepagio.contato@gmail.com>
2025-06-05 12:02:07 -0300

    feat: secao 11 Revendo o useState

---
+++ 11_HOOKS/reacthooks/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import HookUseState from "../components/HookUseState";
+
+const Home = () => {
+  return (
+    <div>
+      <HookUseState></HookUseState>
+    </div>
+  );
+};
+
+export default Home;
