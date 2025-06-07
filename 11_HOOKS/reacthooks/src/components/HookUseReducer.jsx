2b830bf18e61ea37e8c2d48411d30b19684794c2
gPagio <guilhermepagio.contato@gmail.com>
2025-06-07 13:36:27 -0300

    feat: secao 11 useReducer com actions

--- 11_HOOKS/reacthooks/src/components/HookUseReducer.jsx
+++ 11_HOOKS/reacthooks/src/components/HookUseReducer.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 const HookUseReducer = () => {
   // 1 - começando com o useReducer
@@ -6,11 +6,67 @@ const HookUseReducer = () => {
     return Math.random();
   });
 
+  // 2 - avançando no useReducer
+  const initialTasks = [
+    { id: 1, text: "Estudar React" },
+    { id: 2, text: "Estudar JavaScript" },
+    { id: 3, text: "Estudar TypeScript" },
+  ];
+
+  const taskReducer = (state, action) => {
+    switch (action.type) {
+      case "ADD":
+        const newTask = {
+          id: Math.random(),
+          text: taskText,
+        };
+
+        setTaskText("");
+
+        return [...state, newTask];
+      
+      case "DELETE":
+        return state.filter((task) => task.id !== action.id);
+      default:
+        return state;
+    }
+  };
+
+  const [taskText, setTaskText] = useState("");
+  const [tasks, dispatchTasks] = useReducer(taskReducer, initialTasks);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatchTasks({
+      type: "ADD",
+    });
+  };
+
+  const removeTask = (id) => {
+    dispatchTasks({
+      type: "DELETE",
+      id,
+    });
+  }
+
   return (
     <div>
       <h2>useReducer</h2>
       <p>Gerando um número aleatório: {number}</p>
       <button onClick={() => dispatch()}>Gerar número</button>
+      <h3>Tarefas:</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTaskText(e.target.value)}
+          value={taskText}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
+      {tasks.map((task) => (
+        <li key={task.id} onDoubleClick={() => removeTask(task.id)} >{task.text}</li>
+      ))}
       <hr />
     </div>
   );
