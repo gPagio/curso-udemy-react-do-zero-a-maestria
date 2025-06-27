72c61dc9bbe8e9f8fbe7a558abfc27d1bdeabffa
gPagio <guilhermepagio.contato@gmail.com>
2025-06-27 15:41:16 -0300

    feat: secao 12 Validacoes de criacao de usuario

--- 12_REACTGRAM/backend/routes/UserRoutes.js
+++ 12_REACTGRAM/backend/routes/UserRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const { register } = require("../controllers/UserController");
 
+// Middlewares
 const { validate } = require("../middlewares/handleValidation");
+const { userCreateValidation } = require("../middlewares/useValidations");
 
-router.post("/register", validate, register);
+router.post("/register", userCreateValidation(), validate, register);
 
 module.exports = router;
