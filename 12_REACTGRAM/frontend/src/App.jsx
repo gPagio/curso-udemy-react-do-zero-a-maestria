fc949c76383a778c30b8747276f0de68272f510c
gPagio <guilhermepagio.contato@gmail.com>
2025-07-08 17:21:07 -0300

    feat: secao 12 commit parcial

--- 12_REACTGRAM/frontend/src/App.jsx
+++ 12_REACTGRAM/frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
 
+// Router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <h1>ReactGram</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
