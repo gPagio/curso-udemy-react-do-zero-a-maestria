7c337be800cb4d06936208c8dca19341d846fb1f
gPagio <guilhermepagio.contato@gmail.com>
2025-07-08 22:59:33 -0300

    feat: secao 12 Criando formulario de cadastro

--- 12_REACTGRAM/frontend/src/pages/Auth/Register.jsx
+++ 12_REACTGRAM/frontend/src/pages/Auth/Register.jsx
@@ -1,5 +1,32 @@
+import "./Auth.css";
+
+// React Components
+import { Link } from "react-router-dom";
+
+// Hooks
+import { useState, useEffect } from "react";
+
 const Register = () => {
-  return <div>Register</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos de seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem uma conta? <Link to="/login">Clique aqui.</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Register;
