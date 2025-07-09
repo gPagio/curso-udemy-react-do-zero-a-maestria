8255af3ce0579a53ed7be587d991c77628e0c9f8
gPagio <guilhermepagio.contato@gmail.com>
2025-07-09 11:25:33 -0300

    feat: secao 12 Criando service de autenticacao

--- 12_REACTGRAM/frontend/src/services/authService.js
+++ 12_REACTGRAM/frontend/src/services/authService.js
@@ -1,14 +1,24 @@
 import { api, requestConfig } from "../utils/config";
 
 // Rgeistrar um usuario
-const registerUser = async(data) => {
-
+const registerUser = async (data) => {
   const config = requestConfig("POST", data);
 
   try {
-    const res = await fetch(api + "/user/register", config).then();
+    const res = await fetch(api + "/user/register", config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    if (res) {
+      localStorage.setItem("user", JSON.stringify(res));
+    }
   } catch (error) {
     console.log(error);
   }
+};
 
-}
+const authService = {
+  registerUser,
+};
+
+export default authService;
