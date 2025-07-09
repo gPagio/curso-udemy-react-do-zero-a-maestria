bf8dc425f2c1aad6c4195acce3e37ce1bc97e975
gPagio <guilhermepagio.contato@gmail.com>
2025-07-09 10:42:23 -0300

    feat: secao 12 Commit parcial

--- 12_REACTGRAM/frontend/src/services/authService.js
+++ 12_REACTGRAM/frontend/src/services/authService.js
@@ -1,2 +1,14 @@
 import { api, requestConfig } from "../utils/config";
 
+// Rgeistrar um usuario
+const registerUser = async(data) => {
+
+  const config = requestConfig("POST", data);
+
+  try {
+    const res = await fetch(api + "/user/register", config).then();
+  } catch (error) {
+    console.log(error);
+  }
+
+}
