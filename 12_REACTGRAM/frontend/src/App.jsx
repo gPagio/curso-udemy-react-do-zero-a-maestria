ca1e463914766926a99db296f8548243d1ac812a
gPagio <guilhermepagio.contato@gmail.com>
2025-07-08 22:18:01 -0300

    feat: secao 12 Configuranto React Router

--- 12_REACTGRAM/frontend/src/App.jsx
+++ 12_REACTGRAM/frontend/src/App.jsx
@@ -3,12 +3,19 @@ import "./App.css";
 // Router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+// Pages
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/"></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
