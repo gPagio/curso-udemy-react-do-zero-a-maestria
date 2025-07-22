469b820718ac2f730099257973d424b24db21593
gPagio <guilhermepagio.contato@gmail.com>
2025-07-22 08:56:54 -0300

    feat: secao 12 Service de login

--- 12_REACTGRAM/frontend/src/services/authService.js
+++ 12_REACTGRAM/frontend/src/services/authService.js
@@ -25,6 +25,25 @@ const logoutUser = () => {
   localStorage.removeItem("user");
 };
 
+const loginUser = async (data) => {
+  const config = requestConfig("POST", data);
+
+  try {
+    const res = await fetch(api + "users/login", config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    if (res) {
+      // Salva item no localStorage do navegador
+      localStorage.setItem("user", JSON.stringify(res));
+    }
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const authService = {
   registerUser,
   logoutUser,
