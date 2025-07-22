9e4811907a8059968fa24baf41a5c7b2e50c7def
gPagio <guilhermepagio.contato@gmail.com>
2025-07-22 08:50:53 -0300

    feat: secao 12 Criando form de login

--- 12_REACTGRAM/frontend/src/pages/Auth/Login.jsx
+++ 12_REACTGRAM/frontend/src/pages/Auth/Login.jsx
@@ -1,5 +1,49 @@
+import "./Auth.css";
+
+// Components
+import { Link } from "react-router-dom";
+import Message from "../../components/Message";
+
+// Hooks
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
+
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {};
+
+  return (
+    <div id="login">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Faça login para ver o que há de novo.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ""}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ""}
+          required
+        />
+        <button className="btn" type="submit">
+          Entrar
+        </button>
+        <p>
+          Não tem uma conta? <Link to="/register">Crie uma!</Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
