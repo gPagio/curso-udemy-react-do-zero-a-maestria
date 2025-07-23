d66072b63e89ee9673bac7b52bcc589feac11b7f
gPagio <guilhermepagio.contato@gmail.com>
2025-07-23 17:30:08 -0300

    feat: secao 12 Estruturando pagina de edicao de perfil

--- 12_REACTGRAM/frontend/src/App.jsx
+++ 12_REACTGRAM/frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
+import EditProfile from "./pages/EditProfile/EditProfile";
 
 // Components
 import Navbar from "./components/Navbar";
@@ -32,11 +33,15 @@ function App() {
             ></Route>
             <Route
               path="/login"
-              element={!auth ? <Login /> : <Navigate to={"/home"} />}
+              element={!auth ? <Login /> : <Navigate to={"/"} />}
             ></Route>
             <Route
               path="/register"
-              element={!auth ? <Register /> : <Navigate to={"/home"} />}
+              element={!auth ? <Register /> : <Navigate to={"/"} />}
+            ></Route>
+            <Route
+              path="/profile"
+              element={auth ? <EditProfile /> : <Navigate to={"/login"} />}
             ></Route>
           </Routes>
         </div>
