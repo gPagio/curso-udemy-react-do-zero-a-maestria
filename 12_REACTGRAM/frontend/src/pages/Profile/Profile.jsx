a8b8622e1dd9c6f2574a0f84b332cddda1cd7875
gPagio <guilhermepagio.contato@gmail.com>
2025-08-04 17:06:10 -0300

    feat: secao 12 Exibindo dados no perfil

--- 12_REACTGRAM/frontend/src/pages/Profile/Profile.jsx
+++ 12_REACTGRAM/frontend/src/pages/Profile/Profile.jsx
@@ -11,9 +11,36 @@ import { BsFillEyeFill, BsPencilFill, BsXLg } from "react-icons/bs";
 import { useState, useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { getUserDetails } from "../../slices/userSlice";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  const { user, loading } = useSelector((state) => state.user);
+  const { user: userAuth } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(getUserDetails(id));
+  }, [dispatch, id]);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  return (
+    <div id="profile">
+      <div className="profile-header">
+        {user.profileImage && (
+          <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
+        )}
+        <div className="profile-description">
+          <h2>{user.name}</h2>
+          <p>{user.bio}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
