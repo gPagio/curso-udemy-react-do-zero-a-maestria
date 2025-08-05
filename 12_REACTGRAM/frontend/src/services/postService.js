38002e4c422290ea13956e1ac597ada4928e3187
gPagio <guilhermepagio.contato@gmail.com>
2025-08-05 11:39:25 -0300

    feat: secao 12 Service de edicao de fotos

--- 12_REACTGRAM/frontend/src/services/postService.js
+++ 12_REACTGRAM/frontend/src/services/postService.js
@@ -42,6 +42,20 @@ const deletePost = async (id, token) => {
   }
 };
 
-const postService = { publishPost, getUserPosts, deletePost };
+const updatePost = async (id, data, token) => {
+  const config = requestConfig("PUT", data, token);
+
+  try {
+    const res = await fetch(api + "/posts/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const postService = { publishPost, getUserPosts, deletePost, updatePost };
 
 export default postService;
