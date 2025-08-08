f4f889d7a186e72967706e1a093b2faf6b862913
gPagio <guilhermepagio.contato@gmail.com>
2025-08-08 10:23:33 -0300

    feat: secao 18 Criando formulario

--- 18_TODO_COM_TS/todo_react_com_ts/src/App.tsx
+++ 18_TODO_COM_TS/todo_react_com_ts/src/App.tsx
@@ -1,6 +1,8 @@
 // Components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 
 // CSS
 import styles from "./App.module.css";
@@ -10,7 +12,14 @@ function App() {
     <div>
       <Header />
       <main className={styles.main}>
-        <h1>Conteudo...</h1>
+        <div>
+          <h2>Oque você vai fazer?</h2>
+          <TaskForm btnText="Criar Tarefa" />
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList />
+        </div>
       </main>
       <Footer />
     </div>
