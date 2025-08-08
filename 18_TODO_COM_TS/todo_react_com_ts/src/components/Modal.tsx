3f2c0447714310f43e2a1b5a724e166cce6de97c
gPagio <guilhermepagio.contato@gmail.com>
2025-08-08 12:53:12 -0300

    feat: secao 18 Estilizando model

--- 18_TODO_COM_TS/todo_react_com_ts/src/components/Modal.tsx
+++ 18_TODO_COM_TS/todo_react_com_ts/src/components/Modal.tsx
@@ -1,12 +1,15 @@
 // CSS
 import styles from "./Modal.module.css";
 
-const Modal = () => {
+type Props = { children: React.ReactNode };
+
+const Modal = ({ children }: Props) => {
   return (
     <div id="modal">
-      <div></div>
-      <div>
+      <div className={styles.fade}></div>
+      <div className={styles.modal}>
         <h2>Texto Modal</h2>
+        {children}
       </div>
     </div>
   );
