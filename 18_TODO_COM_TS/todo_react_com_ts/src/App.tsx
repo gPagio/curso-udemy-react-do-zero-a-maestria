01b1759575671caf1ea253b6c21bf1eb872876d2
gPagio <guilhermepagio.contato@gmail.com>
2025-08-08 10:03:50 -0300

    feat: secao 18 Estilos iniciais

--- 18_TODO_COM_TS/todo_react_com_ts/src/App.tsx
+++ 18_TODO_COM_TS/todo_react_com_ts/src/App.tsx
@@ -2,11 +2,16 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+// CSS
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
       <Header />
-      <h1>Conteudo...</h1>
+      <main className={styles.main}>
+        <h1>Conteudo...</h1>
+      </main>
       <Footer />
     </div>
   );
