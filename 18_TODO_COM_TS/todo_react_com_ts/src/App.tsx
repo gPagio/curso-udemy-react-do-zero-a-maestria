5a1cbaa5555fb3a9648ccb50df7279d3485cff3b
gPagio <guilhermepagio.contato@gmail.com>
2025-08-08 09:35:50 -0300

    feat: secao 18 Criando header e footer

--- 18_TODO_COM_TS/todo_react_com_ts/src/App.tsx
+++ 18_TODO_COM_TS/todo_react_com_ts/src/App.tsx
@@ -1,7 +1,15 @@
+// Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-
-  return <div>TODO REACT</div>;
+  return (
+    <div>
+      <Header />
+      <h1>Conteudo...</h1>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ 18_TODO_COM_TS/todo_react_com_ts/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <span>React + TS Todo</span> &copy; {new Date().getFullYear()}
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
